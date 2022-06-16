da8777787b0618d694dace0aeffa0edc86925483
Alaistair Chan <alaistair@gmail.com>
2022-06-16 23:43:56 +1000

    add barbell weight allocation;

--- workout.py
+++ workout.py
@@ -16,41 +16,129 @@ def main():
     con = sqlite3.connect(f"{PATH}silver.sqlite")
     cursor = con.cursor()
 
-    st.write(get_flat_table(con))
-
     # Get workout names
     cursor.execute("SELECT DISTINCT(Workout_Name) FROM Workout_Set")
     workout_names = cursor.fetchall()
-    df_workout_names = pd.DataFrame(workout_names)
-
+    # Flatten nested list
+    workout_names = [element for sublist in workout_names for element in sublist]
     col1, col2, col3, = st.columns((1,1,1))
-    col1.write("#### Workout")
-    col2.write("#### Last")
-    col3.write("#### Add")
 
-    col1, col2, col3, col4, col5, col6 = st.columns((1,1,1,1,1,1))
-    col2.write("#### Latest")
-    col3.write("#### Weight")
-    col4.write("#### Reps")
-    col5.write("#### Weight")
-    col6.write("#### Reps")
-    col1_workout, col2_latest, col3_weight, col4_reps, col5_weight_add, col6_x = st.columns((1,1,1,1,1,1))
+    workout_radio = col1.radio('', workout_names)
+    workout_name = workout_radio
+    last_workout_str, df_last_workout = get_last_workout(workout_name, con)
+    last_workout_date = datetime.datetime.strptime(last_workout_str, "%Y-%m-%d %H:%M:%S")
+    last_workout_date = last_workout_date.date()
+
+    col2.write("## " + workout_name)
+    col2.write("Latest workout")
+    col2.write(last_workout_date)
+    col2.write("Weight")
+    col2.write(df_last_workout[0][1])
+    col2.write("Reps")
+    col2.write(df_last_workout[0][2])
+    col2.write("Sets")
+
+    col3.write("### .")
+    weight = col3.number_input("Weight", min_value=0, value=int(df_last_workout[0][1]), key=workout_name[0] + 'weight')
+    col3.number_input('Reps', min_value=0, value=int(df_last_workout[0][2]), key=workout_name[0])
+    col3.button("Add set")
+
+
+    with st.expander("See all workouts"):
+        col1, col2, col3, = st.columns((1,1,1))
+        col1.write("#### Workout")
+        col2.write("#### Last")
+        col3.write("#### Add")
+
+        col1, col2, col3, col4, col5, col6 = st.columns((1,1,1,1,1,1))
+        col2.write("#### Latest")
+        col3.write("#### Weight")
+        col4.write("#### Reps")
+        col5.write("#### Weight")
+        col6.write("#### Reps")
+        col1_workout, col2_latest, col3_weight, col4_reps, col5_weight_add, col6_x = st.columns((1,1,1,1,1,1))
+
+        for workout_name in workout_names:
+            last_workout_date, df_last_workout = get_last_workout(workout_name, con)
+            col1_workout.write(workout_name)
+            col2_latest.write(last_workout_date)
+            col3_weight.write(df_last_workout[0][1])
+            col4_reps.write(df_last_workout[0][2])
+
+    with st.expander("See history"):
+        st.write(get_flat_table(con))
 
 
 
-    for workout_name in workout_names:
-        last_workout_date, df_last_workout = get_last_workout(workout_name[0], con)
-        col1_workout.write(workout_name[0])
-        col2_latest.write(last_workout_date)
-        col3_weight.write(df_last_workout[0][1])
-        col4_reps.write(df_last_workout[0][2])
-        try:
-            col5_weight_add.number_input('', min_value=0, value=int(df_last_workout[0][1]), key=workout_name[0])
-        except Exception:
-            pass
+
     con.close()
 
+    st.write("## Barbell weight allocation")
+    weight_to_lift = st.number_input("Weight to lift", min_value=0.0, value=float(weight))
+    weight_bar = st.number_input("Weight of bar", min_value=0, value=16)
+    weight_set_full = {20.0: 2, 10.0: 2, 5.0: 2, 2.5: 2, 1.0: 2, 0.75: 2, 0.5: 2, 0.25:2}
+    weight_set_to_use_full, weight_unallocated = calculate_barbell_weights(weight_to_lift, weight_set_full, weight_bar)
 
+    for weight, number in weight_set_to_use_full.items():
+        if number > 0:
+            st.write(f"{str(weight)} weight {str(number)}")
+
+    if weight_unallocated != 0:
+        st.write(f"Unallocated {str(weight_unallocated)}")
+
+
+
+
+
+    #df_weights = weight_allocate_test(weight_set_full, weight_bar)
+    #st.dataframe(df_weights.style.apply(highlight_unallocated, axis=1))
+
+def calculate_barbell_weights(weight_to_lift, weight_set_full, weight_bar):
+   
+    weight_to_allocate = weight_to_lift - weight_bar
+    if weight_to_allocate < 0:
+        return {}, weight_to_allocate
+
+    # Halve weight set because of two sides for barbell
+    weight_set_half = {key: value//2 for key, value in weight_set_full.items()}
+    weight_set_to_use_half, weight_unallocated = allocate_weights(weight_to_allocate/2, weight_set_half)
+
+    # Double weight set for total weight
+    return {key: value * 2 for key, value in weight_set_to_use_half.items()}, weight_unallocated * 2
+
+def allocate_weights(weight_to_allocate, weight_set_full):
+
+    weight_set = {}
+    for weight, number in sorted(weight_set_full.items(), reverse=True):
+        weight_set[weight] = 0
+
+        if (weight_to_allocate >= weight) and number > 0:
+            weight_set[weight] = min((weight_to_allocate // weight), number)
+            weight_to_allocate -= min((weight_to_allocate // weight), number) * weight
+
+    weight_unallocated = weight_to_allocate
+    return weight_set, weight_unallocated
+
+def total_weight(weight_set, weight_bar):
+    total_weight = weight_bar
+    for weight, number in weight_set.items():
+        total_weight += weight * number
+    return total_weight
+
+def weight_allocate_test(weight_set_full, weight_bar):
+
+    df_weights = pd.DataFrame()
+    for i in range(15, 100):
+        weight_set, weight_unallocated = calculate_barbell_weights(i, weight_set_full, weight_bar)
+        weight_set['Barbell'] = weight_bar
+        weight_set['Total weight'] = i
+        weight_set['Unallocated'] = weight_unallocated
+
+        df_weights = df_weights.append(weight_set, ignore_index=True)
+    return df_weights
+
+def highlight_unallocated(s):
+    return ['background-color: red']*len(s) if s.Unallocated else ['background-color: white']*len(s)
 
 def get_full_table(con):
     df_dates = pd.read_sql_query("SELECT * FROM Dates", con).drop(columns=['index'])
@@ -101,13 +189,7 @@ def get_last_workout(workout_name, con):
 
     return last_workout_date, cur.fetchall()
 
+
+
 if __name__ == '__main__':
     main()
-    padding = 3
-    st.markdown(f""" <style>
-        .reportview-container .main .block-container{{
-            padding-top: {padding}rem;
-            padding-right: {padding}rem;
-            padding-left: {padding}rem;
-            padding-bottom: {padding}rem;
-        }} </style> """, unsafe_allow_html=True)
